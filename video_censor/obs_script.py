46a426fcb91d85f99782698a48a58c5df7f13323
unknown <skalutsky@gmail.com>
2019-03-02 06:52:36 +0300

    code cleaning

--- video_censor/obs_script.py
+++ video_censor/obs_script.py
@@ -1,19 +1,25 @@
 import obspython as obs
 
-def blur():
+
+def blur(pred):
+    #switches blur on if probability is high
+    source = obs.obs_get_source_by_name('blur')
+    state = obs.obs_source_enabled(source)
+    if state and pred < 0.45:
+        obs.obs_source_set_enabled(source, False)
+    if not state and pred > 0.45:
+        obs.obs_source_set_enabled(source, True)
+    obs.obs_source_release(source)
+
+
+def check():
     with open(r'C:\Users\immmaggo\Desktop\final_censor\communicator.txt', 'r') as f:
         try:
             pred = float(f.read())
         except:
             pass
-    source = obs.obs_get_source_by_name('blurr')
-    state = obs.obs_source_enabled(source)
-    if state and pred < 0.4:
-        obs.obs_source_set_enabled(source, False)
-    if not state and pred > 0.4:
-        obs.obs_source_set_enabled(source, True)
-    obs.obs_source_release(source)
+    blur(pred)
 
 # called at startup
 def script_load(settings):
-    obs.timer_add(blur, 15)
+    obs.timer_add(check, 15)
