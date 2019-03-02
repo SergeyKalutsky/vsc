4759a13023f37e37b77b0d22adce2f2fece9ffb4
Sergey Kalutsky <34448296+SergeyKalutsky@users.noreply.github.com>
2019-03-02 05:57:27 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='video_censor',
+      version='0.0.1',
+      description='Real time video recording censor',
+      url='https://github.com/SergeyKalutsky/video_censor',
+      author='Sergey Kalutsky',
+      author_email='skalutsky@gmail.com',
+      license='MIT',
+      packages=['video_censor'],
+      zip_safe=False)
