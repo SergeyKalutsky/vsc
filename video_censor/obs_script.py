63b1869bb8fc9ffc3a96f26f4428394b8c1d5e11
Sergey Kalutsky <34448296+SergeyKalutsky@users.noreply.github.com>
2019-03-02 06:08:38 +0300

    Update obs_script.py

--- video_censor/obs_script.py
+++ video_censor/obs_script.py
@@ -1,15 +1,13 @@
 import obspython as obs
-import os
 
 
-def blurr():
 
+def blur():
     with open(r'C:\Users\immmaggo\Desktop\final_censor\communicator.txt', 'r') as f:
         try:
             pred = float(f.read())
         except:
             pass
-
     source = obs.obs_get_source_by_name('blurr')
     state = obs.obs_source_enabled(source)
     if state and pred < 0.4:
@@ -18,8 +16,6 @@ def blurr():
         obs.obs_source_set_enabled(source, True)
     obs.obs_source_release(source)
 
-    pred1 = pred
-
 # called at startup
 def script_load(settings):
-    obs.timer_add(blurr, 15)
+    obs.timer_add(blur, 15)
