e0d916e76cae8ce28da708f5f402a5cfb855bdb4
Sergey Kalutsky <skalutsky@gmail.com>
2019-03-14 18:57:40 +0300

    support for any monitor

--- video_censor/classifier.py
+++ video_censor/classifier.py
@@ -12,37 +12,41 @@ model = keras.models.load_model('mobilenet1.3.h5')
 def preprocess_img(screen_part):
     #Generate and preprocess screenshot
     while True:
-        img = np.asarray(sct.grab(screen_part))[:,:,:3]
-        img =  cv2.resize(img, (224, 224)) / 255.0 
-        yield img
+    	img = np.asarray(sct.grab(screen_part))[:,:,:3]
+    	img =  cv2.resize(img, (224, 224)) / 255.0 
+    	yield img
 
 
 def predict(img):
-    pred = model.predict(np.asarray([img]))[0][0]
-    return pred
+	pred = model.predict(np.asarray([img]))[0][0]
+	return pred
 
 
-def update_monitor(sct, coor):
-    if sct.monitors[1] != coor:
-        sct.monitors[1] = coor
+def update_monitor(monitor, screen_part):
+	monitor["top"] += screen_part["top"]
+	monitor["left"] += screen_part["left"]
+	monitor["width"] = screen_part["width"]
+	monitor["height"] = screen_part["height"]
+	return monitor
 
 
 def connect(port):
-    context = zmq.Context()
-    socket = context.socket(zmq.REQ)
-    socket.connect(f"tcp://localhost:{port}")
-    return socket
+	context = zmq.Context()
+	socket = context.socket(zmq.REQ)
+	socket.connect(f"tcp://localhost:{port}")
+	return socket
 
 
 if __name__=='__main__':
-    port = 5557
-    socket = connect(port)
-    socket.send_json({"action": "get screen region"})
-    screen_part = socket.recv_json()
-    with mss.mss() as sct:
-        for img in preprocess_img(screen_part):
-            pred = predict(img)
-            print(pred)
-            socket.send_json({"action": "update screen",
-                              "prediction": float(pred)})
-            socket.recv()
+	port = 5557
+	socket = connect(port)
+	socket.send_json({"action": "get screen region"})
+	screen_part = socket.recv_json()
+	with mss.mss() as sct:
+		screen_part = update_monitor(sct.monitors[2], screen_part)
+		for img in preprocess_img(screen_part):
+			pred = predict(img)
+			print(pred)
+			socket.send_json({"action": "update screen",
+				"prediction": float(pred)})
+			socket.recv()
