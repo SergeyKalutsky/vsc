edbc5c1260ab9026a8009e84b46553074c748a2b
Sergey Kalutsky <skalutsky@gmail.com>
2019-03-26 03:25:17 +0300

    refactor to single cmd command

--- video_censor/classifier.py
+++ video_censor/classifier.py
@@ -4,14 +4,7 @@ import mss
 import json
 import argparse
 import numpy as np
-from .monitor import Monitor
-
-
-ROOT_DIR = path = os.path.dirname(os.path.realpath(__file__))
-parser = argparse.ArgumentParser()
-parser.add_argument("-v", help="verbose output", action="store_true")
-parser.add_argument("--scrshot", help="make screenshot of a selected region", action="store_true")
-parser.add_argument("--port", help="socket port", type=int, default=5557)
+from monitor import Monitor
 
 
 def connect(port, socket_type=zmq.REQ):
@@ -21,25 +14,46 @@ def connect(port, socket_type=zmq.REQ):
     return socket
 
 
-def main():
-    args = parser.parse_args()
+def check_monitor_region():
+    answ = input('Is monitor region correct?:[y/n]').lower()
+    if answ == 'y':
+        return True
+    elif answ == 'n':
+        print('Check if OBS script settings are correct and try again')
+        return False
+    else:
+        print('Invalid input try again')
+        return check_monitor_region()
+
+
+def run_classifier(monitor, socket):
+    from tensorflow import keras
+
     verboseprint = print if args.v else lambda *args, **kwargs: None
+    model = keras.models.load_model(os.path.join(ROOT_DIR, 'mobilenet1.3.h5'))
+    for img in monitor.screenshot():
+        pred = model.predict(np.asarray([img]))[0][0]
+        socket.send_pyobj({'msg': 'predict', 'pred': pred})
+        socket.recv_pyobj()
+        verboseprint(pred)
+
+
+def main():
     socket = connect(args.port)
     socket.send_pyobj({'msg': 'screen'})
     mon_info = socket.recv_pyobj()
     with mss.mss() as sct:
         monitor = Monitor(sct, mon_info)
-        if args.scrshot:
-            monitor.test_screenshot()
-        else:
-            from tensorflow import keras
-            
-            model = keras.models.load_model(os.path.join(ROOT_DIR, 'mobilenet1.3.h5'))
-            for img in monitor.screenshot():
-                pred = model.predict(np.asarray([img]))[0][0]
-                socket.send_pyobj({'msg': 'predict', 'pred': pred})
-                socket.recv_pyobj()
-                verboseprint(pred)
+        monitor.test_screenshot()
+        correct = check_monitor_region()
+        if correct:
+            run_classifier(monitor, socket)
+
 
 if __name__=='__main__':
+    ROOT_DIR = path = os.path.dirname(os.path.realpath(__file__))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", help="verbose output", action="store_true")
+    parser.add_argument("--port", help="socket port", type=int, default=5557)
+    args = parser.parse_args()
     main()
